fa210108c468d6db8c03b7d6beef9c0eb11017f2
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-04-01 10:58:52 +0200

    feat: milestone 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const products = [
   { name: "Mela", price: 0.5 },
@@ -9,6 +9,8 @@ const products = [
 ];
 
 const App = () => {
+  const [addedProducts, setAddedProducts] = useState([]);
+
   const [state, dispatch] = useReducer((state, action) => {
     switch (action.type) {
       case "ADD":
@@ -20,15 +22,39 @@ const App = () => {
     }
   }, []);
 
+  const addToCart = (product) => {
+    product = { ...product, quantity: 1 };
+    const productExists = addedProducts.find(
+      (item) => item.name === product.name
+    );
+    if (!productExists) {
+      setAddedProducts([...addedProducts, { ...product, quantity: 1 }]);
+    }
+  };
   return (
     <>
       <h1>Lista della Spesa:</h1>
       {products.map((product) => (
         <div key={product.name}>
           <span>{product.name} :</span>
-          <span> {product.price} euro</span>
+          <span> {product.price} euro </span>
+          <button onClick={() => addToCart(product)}>
+            Aggiungi al carrello
+          </button>
         </div>
       ))}
+      {addedProducts.length > 0 && (
+        <>
+          <h1>Carrello:</h1>
+          {addedProducts.map((product) => (
+            <div key={product.name}>
+              <span>{product.name} : </span>
+              <span>{product.price} euro -</span>
+              <span>x{product.quantity} </span>
+            </div>
+          ))}
+        </>
+      )}
     </>
   );
 };
